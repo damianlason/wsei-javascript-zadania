c15dcfa125ff4a3ab2ee6561198cb8c93c0637d1
Lasoń Damian <damian.lason@microsoft.wsei.edu.pl>
2019-10-06 15:15:16 +0200

    zadanie 1

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,11 @@
+function countHello(number){
+    var counter = 1;
+    var interval = setInterval(()=>{
+        if(counter == number){
+            clearInterval(interval);
+        }
+        console.log("Hello", counter);
+        counter++;
+    }, 500)
+}
+countHello(5);
