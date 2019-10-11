bd630a46290be539801f058608033fbec75a14da
Damian Lasoń <lasondamian9@gmail.com>
2019-10-11 16:34:43 +0200

    Multidimensional arrays ex 2

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -39,8 +39,26 @@ console.log(task1Array[3][1]);
 var task2Array = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
-    [9, 10, 11, 12]
+    [9, 10, 11,12]
 ];
+var length = task2Array.length;
+
+console.log("Pierwszy wymiar tablicy:");
+for(var i=0; i<length;i++){
+  console.log(task2Array[i]);
+}
+
+console.log("Długość tablic drugiego wymiaru:")
+for(var i=0; i<length; i++){  
+  console.log(task2Array[i].length);  
+}
+
+console.log("Drugi wymiar tablicy:");
+for(var i=0; i<length; i++){
+  for(var j=0; j<task2Array[i].length; j++){
+    console.log(task2Array[i][j]);
+  }
+}   
 
 //ZADANIE 3
 
@@ -58,3 +76,5 @@ function print2DArray(array){
 
 var table = [[1], [2,3,4,5,6], [7,8,9,10,11,12,13]];
 print2DArray(table);
+
+//ZADANIE 5
