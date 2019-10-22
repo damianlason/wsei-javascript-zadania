802581870182f8a9dc22a422cc9a85f3359c04c9
Damian Lasoń <lasondamian9@gmail.com>
2019-10-22 17:45:36 +0200

    this

--- 1_Zadania/obowiązkowe/5_this/js/app.js
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -15,3 +15,35 @@ var car = {
 car.printCarInfo();
 car.drive(20);
 car.printCarInfo();
+
+//ZADANIE 1
+
+car.inspectionDates = ["20.10.2015","20.10.2016","20.10.2017"];
+car.newInspectionDate = function(newDate) {
+    this.inspectionDates.push(newDate);
+}
+car.showInspectionDates = function(){
+    var length = this.inspectionDates.length;
+    for(var i = 0; i<length; i++){
+        console.log(this.inspectionDates[i]);
+    }
+}
+
+car.newInspectionDate("20.10.2018");
+car.newInspectionDate("20.10.2019");
+car.showInspectionDates();
+
+//ZADANIE 3
+
+var stairs = {
+    step: 0,
+    up:function(){
+        this.step++;
+    },
+    down:function(){
+        this.step--;
+    },
+    printStep:function(){
+        console.log(this.step);
+    }
+}
