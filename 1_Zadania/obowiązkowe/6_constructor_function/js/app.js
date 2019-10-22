6c3f3fdf17735583aee3e8e66dc544f57eef5b90
Damian Lasoń <lasondamian9@gmail.com>
2019-10-22 20:10:45 +0200

    constructor ex 0

---
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -0,0 +1,35 @@
+//ZADANIE 0
+
+function Basket(){
+    this.products = [];
+    this.sum = 0;
+
+    this.addProduct = function(name, price){
+        var product = {
+          name: name,
+          price: price
+        }
+        this.products.push(product);
+    };
+    
+    this.showBasket = function(){
+        console.log("Paragon:");
+        for (var i = 0; i<this.products.length; i++){
+          console.log(this.products[i].name + ": " + this.products[i].price + "zł");
+          this.sum += this.products[i].price;
+        }
+        console.log("Łączna kwota do zapłaty: " + this.sum +"zł");
+        console.log("-----------------------------------");
+    };
+}
+
+var aliceBasket = new Basket();
+aliceBasket.addProduct("pomidor",10);
+aliceBasket.addProduct("arbuz",40);
+aliceBasket.showBasket();
+
+var bruceBasket = new Basket();
+bruceBasket.addProduct("rice", 10);
+bruceBasket.addProduct("grzyby mun",50);
+bruceBasket.addProduct("tofu",20);
+bruceBasket.showBasket();
