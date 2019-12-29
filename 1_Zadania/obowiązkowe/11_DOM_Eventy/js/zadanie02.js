17f38e627168806c78f1f279ac7a3a7b78aefcf8
Damian Lasoń <lasondamian9@gmail.com>
2019-12-29 18:26:12 +0100

    events ex2

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie02.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var btn = document.getElementById("mainBtn");
+    btn.addEventListener('click',function(){
+        console.log("Hura! Działa!");
+    })   
+});
