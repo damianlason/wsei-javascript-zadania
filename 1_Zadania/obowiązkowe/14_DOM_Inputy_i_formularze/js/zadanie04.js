05663e0c74ef5d0b85fb4cc44d3f72fc3eed233a
damianlason <lasondamian9@gmail.com>
2020-01-31 15:45:48 +0100

    card validation

--- 1_Zadania/obowiązkowe/14_DOM_Inputy_i_formularze/js/zadanie04.js
+++ 1_Zadania/obowiązkowe/14_DOM_Inputy_i_formularze/js/zadanie04.js
@@ -3,68 +3,43 @@ document.addEventListener("DOMContentLoaded", function(){
     var name = document.getElementById("name"),
         type = document.getElementById("type");        
     
-        name.addEventListener('change', CheckType);
+        name.addEventListener('change', check);
         
-    function CheckType(){
-        typeOfCard = "";
+    function check(){
+
         if(name.value[0] == "4"){
-            type.innerText = "Visa";
-            typeOfCard = "Visa";
-            name.addEventListener('change', CheckColor);
+            type.innerText = "Visa";  
+                if(name.value.length >=13 && name.value.length <=16){
+                    name.style.border = "solid 3px green";
+                }
+                else if(name.value.length <13 || name.value.length > 16){
+                    name.style.border = "1px solid #ccc";
+                }            
         }
         else if(name.value[0] == "5"){
             type.innerText = "Mastercard";
-            typeOfCard = "MasterCard";
-            name.addEventListener('change', CheckColor);
+                if(name.value.length == 16){
+                    name.style.border = "solid 3px green";
+                }
+                else if(name.value.length < 16 || name.value.length > 16){
+                    name.style.border = "1px solid #ccc";
+                }             
         }
         else if(name.value[0] == "3" && name.value[1] == null){
             type.innerText = "American Express";
-            typeOfCard = "AmericanExpress";
-            name.addEventListener('change', CheckColor);
         }
         else if(name.value[0] == "3" && name.value[1] == "4" || name.value[0] == "3" && name.value[1] == "7") {
             type.innerText = "American Express";
-            typeOfCard = "AmericanExpress";
-            name.addEventListener('change', CheckColor);                      
-        }        
-        else{
-            console.log("test");
-            type.innerText = "";
-            name.style.border = "none";
-            console.log("test2");
-            typeOfCard = "other";
-            // name.addEventListener('change', CheckColor);
-        }
-    }
-
-    function CheckColor(){
-        if(typeOfCard = "Visa"){
-            if(name.value.length >=13 && name.value.length <=16){
-                name.style.border = "solid 3px green";
-            }
-            else{
-                name.style.border = "";
-            }
-        }
-        else if(typeOfCard = "MasterCard"){
-            if(name.value.length == 16){
-                name.style.border = "solid 3px green";
-            }
-            else{
-                name.style.border = "";
-            } 
-        }
-        else if(typeOfCard = "AmericanExpress"){
             if(name.value.length == 15){
                 name.style.border = "solid 3px green";
             }
-            else{
-                name.style.border = "";
-            } 
-        }
-        else{
-            name.style.border = "none";
+            else if(name.value.length < 15 || name.value.length > 15){
+                name.style.border = "1px solid #ccc";
+            }
         }        
+        else{ 
+            type.innerText = "";
+            name.style.border = "1px solid #ccc"; 
+        }
     }
-    
 });
